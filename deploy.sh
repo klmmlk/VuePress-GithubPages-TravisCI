2b9ed7ef64baade878d0b9f51e7f94c18aea8b14
klmmlk <jiong-lin@live.com>
2022-06-20 13:19:12 +0800

    233

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -e
-yarn docs:build
-cd docs/.vuepress/dist
-git init
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:klmmlk/VuePress-GithubPages-TravisCI.git master:gh-pages
-cd -
